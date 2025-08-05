838da2eec9d1a4de28d007b3c7c7d352234fb00f
QuanMinhGitHub <minhquan8620042gmail.com>
2025-08-05 22:57:50 +0300

    Update

--- .github/workflows/backend_setup.sh
+++ .github/workflows/backend_setup.sh
@@ -6,9 +6,9 @@ set -e
 cat <<EOT > /home/ubuntu/backend/.env
 PORT=8000
 DB_HOST=localhost
-DB_USER=${DB_USER}
-DB_PASSWORD=${DB_PASSWORD}
-DB_NAME=${DB_NAME}
+DB_USER="${{ secrets.DB_USER }}"
+DB_PASSWORD="${{ secrets.DB_PASSWORD }}"
+DB_NAME="${{ secrets.DB_NAME }}"
 DB_PORT=5432
-JWT_SECRET=${JWT_SECRET}
+JWT_SECRET="${{ secrets.JWT_SECRET }}"
 EOT
